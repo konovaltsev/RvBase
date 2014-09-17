cddd8ae0ec5eb7dc79d0f26f442b85ba6d78592a
Roman Konovaltsev <rkonovaltsev@gmail.com>
2014-09-17 21:31:04 +0400

    Messages translation

---
+++ resources/languages/ru_RU/messages.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'An error occurred during execution; please try again later.' => 'Во время выполнения программы произошла ошибка.',
+    'Page not found.' => 'Страница не найдена',
+    'Page not allowed.' => 'Страница не доступна',
+);
