00fcfec52d8dad2bd6d027ffdfa1d2b247ba18ac
Roman Konovaltsev <konovaltsev@stroytorgi.ru>
2015-11-24 08:49:23 +0300

    Error messages

--- resources/languages/ru_RU/Messages.php
+++ resources/languages/ru_RU/Messages.php
@@ -10,4 +10,7 @@ return array(
     'An error occurred during execution; please try again later.' => 'Во время выполнения программы произошла ошибка.',
     'Page not found.' => 'Страница не найдена',
     'Page not allowed.' => 'Страница не доступна',
+    'An error occurred'    => 'Ошибка',
+    'A 403 error occurred' => 'Ошибка 403',
+    'A 404 error occurred' => 'Ошибка 404',
 );
